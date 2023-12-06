78a21443951232b2d47de88b6ec1194b0602e545
jake-is-ESD-protected <jt@tschavoll.at>
2023-12-06 06:56:53 +0100

    add init routine

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include "jescore.h"
+#include "i2s_io.h"
 
 void setup(){
+    jes_init();
 
+    register_job("i2sinit", I2S_TASK_INIT_MEM_SIZE, I2S_TASK_INIT_PRIORITY, i2s_init, false);
+    register_job("i2srunin", I2S_TASK_RUN_MEM_SIZE, I2S_TASK_RUN_PRIORITY, i2s_run_in, true);
+    register_job("i2srunout", I2S_TASK_RUN_MEM_SIZE, I2S_TASK_RUN_PRIORITY, i2s_run_out, true);
+
+    launch_job("i2sinit");
+    delay(100);
+    launch_job("i2srunin");
+    launch_job("i2srunout");
+    while(1);
 }
 
 void loop(){
