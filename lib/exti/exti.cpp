70d18cb616dc31e9ffb6856d48c64e00158b7cd9
jake-is-ESD-protected <jt@tschavoll.at>
2024-03-15 21:46:09 +0100

    working exti init + handler (debounce needs tweaking)

--- lib/exti/exti.cpp
+++ lib/exti/exti.cpp
@@ -10,14 +10,10 @@
 #include "exti.h"
 #include "pin_defs.h"
 #include <jescore.h>
+#include "fsm.h"
 
-uint32_t last_interrupt_time_rec = 0;
-uint32_t last_interrupt_time_select = 0;
-
-TickType_t pin_button_press_ts = 0;
-TickType_t pin_button_release_ts = 0;
-TickType_t pin_rotary_switch_press_ts = 0;
-TickType_t pin_rotary_switch_release_ts = 0;
+TickType_t pin_button_ts = 0;
+TickType_t pin_rotary_switch_ts = 0;
 TickType_t pin_rotary_A_ts = 0;
 TickType_t pin_rotary_B_ts = 0;
 
@@ -28,7 +24,10 @@ e_err_t __exti_init_pin(gpio_int_type_t edge, gpio_num_t pin, void(*handler)(voi
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = pullup;
     if(gpio_config(&io_conf) != ESP_OK) { return e_syserr_param; }
-    if(gpio_isr_handler_add(pin, handler, (void*)pin) != ESP_OK) { return e_syserr_install; }
+    exti_context_t ctxt;
+    ctxt.context.pin = (uint16_t)pin;
+    ctxt.context.edge = (uint16_t)edge;
+    if(gpio_isr_handler_add(pin, handler, (void*)ctxt.raw) != ESP_OK) { return e_syserr_install; }
     return e_syserr_no_err;
 }
 
@@ -36,28 +35,67 @@ e_err_t __exti_init_pin(gpio_int_type_t edge, gpio_num_t pin, void(*handler)(voi
 e_err_t exti_init(void){
     e_err_t stat;
     if(gpio_install_isr_service(0) != ESP_OK) { return e_syserr_install; }
-    if((stat = __exti_init_pin(GPIO_INTR_ANYEDGE, PIN_BUTTON, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return stat; }
-    if((stat = __exti_init_pin(GPIO_INTR_ANYEDGE, PIN_ROTARY_SWITCH, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return stat; }
-    if((stat = __exti_init_pin(GPIO_INTR_NEGEDGE, PIN_ROTARY_A, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return e_syserr_install; }
-    if((stat = __exti_init_pin(GPIO_INTR_NEGEDGE, PIN_ROTARY_B, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return e_syserr_install; }
+    if((stat = __exti_init_pin(GPIO_INTR_NEGEDGE, PIN_BUTTON, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return stat; }
+    if((stat = __exti_init_pin(GPIO_INTR_POSEDGE, PIN_BUTTON, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return stat; }
+    if((stat = __exti_init_pin(GPIO_INTR_NEGEDGE, PIN_ROTARY_SWITCH, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return stat; }
+    if((stat = __exti_init_pin(GPIO_INTR_POSEDGE, PIN_ROTARY_SWITCH, __exti_handler, GPIO_PULLUP_DISABLE)) != e_syserr_no_err) { return stat; }
+    if((stat = __exti_init_pin(GPIO_INTR_NEGEDGE, PIN_ROTARY_A, __exti_handler, GPIO_PULLUP_ENABLE)) != e_syserr_no_err) { return stat; }
+    if((stat = __exti_init_pin(GPIO_INTR_NEGEDGE, PIN_ROTARY_B, __exti_handler, GPIO_PULLUP_ENABLE)) != e_syserr_no_err) { return stat; }
     return e_syserr_no_err;
 }
 
 
 void IRAM_ATTR __exti_handler(void* p){
-    uint32_t pin = (uint32_t)p;
+    exti_context_t ctxt;
+    ctxt.raw = p;
+    gpio_num_t pin = (gpio_num_t)ctxt.context.pin;
+    gpio_int_type_t edge = (gpio_int_type_t)ctxt.context.edge;
     TickType_t now = xTaskGetTickCountFromISR();
-    switch((gpio_num_t)pin){
+    const char* handler_name = NULL;
+    const char* args = NULL;
+    switch(pin){
         case PIN_BUTTON:
-        
+            if(now - pin_button_ts > EXTI_BUTTON_DEBOUNCE_TICKS){
+                handler_name = FSM_HANDLE_BUTTON_JOB_NAME;
+                if(ctxt.context.edge == GPIO_INTR_NEGEDGE){
+                    args = FSM_HANDLE_BUTTON_SWITCH_ARG_PRESS;
+                }
+                else{
+                    args = FSM_HANDLE_BUTTON_SWITCH_ARG_RELEASE;
+                }
+                pin_button_ts = now;
+            }
+            
         break;
         case PIN_ROTARY_SWITCH:
+            if(now - pin_rotary_switch_ts > EXTI_BUTTON_DEBOUNCE_TICKS){
+                handler_name = FSM_HANDLE_ROTARY_SWITCH_JOB_NAME;
+                if(ctxt.context.edge == GPIO_INTR_NEGEDGE){
+                    args = FSM_HANDLE_BUTTON_SWITCH_ARG_PRESS;
+                }
+                else{
+                    args = FSM_HANDLE_BUTTON_SWITCH_ARG_RELEASE;
+                }
+                pin_rotary_switch_ts = now;
+            }
+            
         break;
         case PIN_ROTARY_A:
+            handler_name = FSM_HANDLE_ROTARY_TURN_JOB_NAME;
+            args = FSM_HANDLE_ROTARY_TURN_ARG_LEFT;
         break;
         case PIN_ROTARY_B:
+            handler_name = FSM_HANDLE_ROTARY_TURN_JOB_NAME;
+            args = FSM_HANDLE_ROTARY_TURN_ARG_RIGHT;
         break;
         default:
         break;
+    }
+    if(handler_name != NULL) { 
+        job_struct_t* pj_to_to = __job_get_job_by_name(handler_name);
+        job_struct_t* pj_core = __job_get_job_by_name("core"); // this is a bit wonky
+        __job_set_args((char*)args, pj_to_to);
+        __job_set_param(&now, pj_to_to);
+        __job_notify_with_job(pj_core, pj_to_to, 1);
     }
 }
--- lib/exti/exti.h
+++ lib/exti/exti.h
@@ -13,6 +13,18 @@
 #include "driver/gpio.h"
 #include "sys_err.h"
 
+#define EXTI_BUTTON_DEBOUNCE_TICKS          500
+#define EXTI_ROTARY_SWITCH_DEBOUNCE_TICKS   500
+
+typedef union {
+    struct {
+        uint16_t pin;
+        uint16_t edge;
+    } context;
+    void* raw;
+} exti_context_t;
+
+
 e_err_t __exti_init_pin(gpio_int_type_t edge, gpio_num_t pin, void(*handler)(void*),gpio_pullup_t pullup);
 
 e_err_t exti_init(void);
