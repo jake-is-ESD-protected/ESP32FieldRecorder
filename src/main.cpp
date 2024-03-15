cd23ab42a4e009c40f212af16ee1197031d89a40
jake-is-ESD-protected <jt@tschavoll.at>
2024-03-15 21:47:11 +0100

    working init of exti and fsm

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h>
 #include "jescore.h"
 #include "i2s_io.h"
+#include "exti.h"
+#include "sys_err.h"
+#include "fsm.h"
+#include "ui.h"
 
 void setup(){
-    jes_init();
+    if(jes_init() != e_err_no_err) { sys_err_handler(); }
 
-    register_job("i2sinit", I2S_TASK_INIT_MEM_SIZE, I2S_TASK_INIT_PRIORITY, i2s_init, false);
-    register_job("i2srunin", I2S_TASK_RUN_MEM_SIZE, I2S_TASK_RUN_PRIORITY, i2s_run_in, true);
-    register_job("i2srunout", I2S_TASK_RUN_MEM_SIZE, I2S_TASK_RUN_PRIORITY, i2s_run_out, true);
+    if(exti_init() != e_syserr_no_err) { sys_err_handler(); }
+    fsm_init();
+    if(ui_init() != e_syserr_no_err) { sys_err_handler(); }
+
+
+    if(register_job(FSM_HANDLE_BUTTON_JOB_NAME, 2048, 1, fsm_handle_button, 0) != e_err_no_err) { sys_err_handler(); }
+    if(register_job(FSM_HANDLE_ROTARY_SWITCH_JOB_NAME, 2048, 1, fsm_handle_rotary_switch, 0) != e_err_no_err) { sys_err_handler(); }
+    if(register_job(FSM_HANDLE_ROTARY_TURN_JOB_NAME, 2048, 1, fsm_handle_rotary_turn, 0) != e_err_no_err) { sys_err_handler(); }
+
+    if(register_job("i2sinit", I2S_TASK_INIT_MEM_SIZE, I2S_TASK_INIT_PRIORITY, i2s_init, false) != e_err_no_err) { sys_err_handler(); }
+    if(register_job("i2srunin", I2S_TASK_RUN_MEM_SIZE, I2S_TASK_RUN_PRIORITY, i2s_run_in, true) != e_err_no_err) { sys_err_handler(); }
+    if(register_job("i2srunout", I2S_TASK_RUN_MEM_SIZE, I2S_TASK_RUN_PRIORITY, i2s_run_out, true) != e_err_no_err) { sys_err_handler(); }
 
     launch_job("i2sinit");
     delay(100);
